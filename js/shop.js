a478948ddea3acb9d25d50f5791e1bf6a6502744
ioana.cora <ioana.cora@casomes.ro>
2019-10-16 20:02:47 +0300

    display cart content from API

--- js/shop.js
+++ js/shop.js
@@ -1,27 +1,27 @@
 window.Shop = {
-  API_BASE_URL: "http://localhost:8085",
+    API_BASE_URL: "http://localhost:8085",
 
-  getProducts: function () {
-      $.ajax({
+    getProducts: function () {
+        $.ajax({
             url: Shop.API_BASE_URL + "/products",
-          method: "GET"
-      }) .done(function (response) {
-          console.log(response);
+            method: "GET"
+        }).done(function (response) {
+            console.log(response);
 
-          Shop.displayPrpducts(response.content);
-      })
-  },
+            Shop.displayProducts(response.content);
+        })
+    },
 
-    displayPrpducts: function(products){
-      var allProductsHtml = "";
+    displayProducts: function (products) {
+        var allProductsHtml = "";
 
-      products.forEach(product => allProductsHtml += Shop.getProductHtml(product));
+        products.forEach(product => allProductsHtml += Shop.getProductHtml(product));
 
-      $(".single-product-area .row").html(allProductsHtml);
+        $(".single-product-area .row").html(allProductsHtml);
     },
 
     getProductHtml: function (product) {
-      return ` <div class="col-md-3 col-sm-6">
+        return ` <div class="col-md-3 col-sm-6">
             <div class="single-shop-product">
             <div class="product-upper">
             <img src="img/product-2.jpg" alt="">
@@ -34,10 +34,42 @@ window.Shop = {
         <div class="product-option-shop">
             <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="${product.id}" rel="nofollow" href="/canvas/shop/?add-to-cart=70">Add to cart</a>
         </div>                       
-        </div>
-        </div>`
+      </div>
+    </div>`
 
+    },
+
+    addProductToCart(productId) {
+        //customerId to be read from memory somehow in the future
+        var customerId = 13;
+        
+        var requestBody = {
+            customerId: customerId,
+            productId: productId
+        };
+        
+        $.ajax({
+            url: Shop.API_BASE_URL + "/carts", 
+            method: "PUT",
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+            window.location.replace("cart.html")
+        })
+    },
+
+    bindEvents: function () {
+        $(".single-product-area .row").delegate(".add_to_cart_button", "click", function (event) {
+           event.preventDefault();
+            // console.log("test");
+           var productId = $(this).data("product_id");
+
+           Shop.addProductToCart(productId);
+
+        })
     }
+
 };
 
 Shop.getProducts();
+Shop.bindEvents();
